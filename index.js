c8059be7231f03f6662876b68540acc9b596a147
CrunchiWaffl3s <crunchiwaffl3s@gmail.com>
2026-06-03 11:23:21 -0400

    changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function switchCatppuccinTheme(themeName) {
+  function switchCatppuccinTheme(themeName) {
     document.documentElement.setAttribute('data-theme', themeName);
-}
+    localStorage.setItem('catppuccin-theme', themeName);
+  }
+
+  const savedTheme = localStorage.getItem('catppuccin-theme');
+  if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+  }
